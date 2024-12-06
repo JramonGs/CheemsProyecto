5161aba8f27aa5248afc763a6e7d77ff0a943c40
JesusG <116382798+JramonGs@users.noreply.github.com>
2024-12-05 20:31:27 -0700

    database

--- CheemsCece/persistence/db.py
+++ CheemsCece/persistence/db.py
@@ -2,10 +2,15 @@ import mysql.connector
 from mysql.connector import Error
 
 def get_db_connection():
-    return mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='Jesus_041104',
-        database='cheems'
-    )   
-    
+    try:
+        connection = mysql.connector.connect(
+            host='127.0.0.1',
+            user='root',
+            password='carlos04',  # Cambia esta contraseña si es necesario
+            database='localhost'  # Asegúrate de que el nombre de la base de datos sea correcto
+        )
+        if connection.is_connected():
+            return connection
+    except Error as e:
+        print("Error while connecting to MySQL", e)
+        return None
